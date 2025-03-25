0a8c3ab885212f4108772fdc41d34b5b2159ec0c
shahid <codershahid2202@gmail.com>
2025-03-25 08:54:46 +0530

    feat: Add Home and About page mappings in HomeController
    - Implemented `/home` endpoint to render the home page.
    - Implemented `/about` endpoint to render the about page.
    - Added title attributes for both pages in the Model.

--- src/main/java/com/example/smartcontactmanager/controller/HomeController.java
+++ src/main/java/com/example/smartcontactmanager/controller/HomeController.java
@@ -1,31 +1,30 @@
 package com.example.smartcontactmanager.controller;
 
+
 import com.example.smartcontactmanager.dao.UserRepository;
 import com.example.smartcontactmanager.entities.Contact;
 import com.example.smartcontactmanager.entities.User;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HomeController {
+@RequestMapping("/home")
+    public String home(Model model){
 
-    @Autowired
-    private UserRepository userRepository;
+    model.addAttribute("title", "Home - Smart Contact Manager");
+    return "home" ;
 
-    @GetMapping("/test")
-    @ResponseBody
-
-    public String test(){
-
-       User user= new User();
-        user.setName("Shahidkhan");
-        user.setEmail("shahid9090@gmail.com");
-
-        Contact contact = new Contact();
-        user.getContacts().add(contact);
-        userRepository.save(user);
-        return "Working";
-    }
 }
+@RequestMapping("/about")
+        public String about(Model model){
+
+            model.addAttribute("title", "About - Smart Contact Manager");
+            return "about" ;
+
+        }
+    }
